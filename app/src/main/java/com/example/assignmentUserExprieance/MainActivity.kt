fccd0fcace25ffaa62636879fbb946f7ee687e98
LENOVO <vbrahmbhatt7420@gmail.com>
2021-12-31 15:19:02 +0530

    Done

--- app/src/main/java/com/example/assignmentUserExprieance/MainActivity.kt
+++ app/src/main/java/com/example/assignmentUserExprieance/MainActivity.kt
@@ -2,33 +2,15 @@ package com.example.assignmentUserExprieance
 
 import android.app.DatePickerDialog
 import android.os.Bundle
-import android.widget.*
+import android.widget.ArrayAdapter
+import android.widget.SeekBar
 import android.widget.SeekBar.OnSeekBarChangeListener
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.textfield.TextInputEditText
+import kotlinx.android.synthetic.main.activity_main.*
 import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var spinnerCountry: Spinner
-    private lateinit var spinnerSscHsc: Spinner
-    private lateinit var spinnerBComBCA: Spinner
-    private lateinit var tetFullName: TextInputEditText
-    private lateinit var tetPhoneNumber: TextInputEditText
-    private lateinit var tetEmailAddress: TextInputEditText
-    private lateinit var tetAddress: TextInputEditText
-    private lateinit var tetBirthDate: TextInputEditText
-    private lateinit var skbPercentageSscHsc: SeekBar
-    private lateinit var chbReading: CheckBox
-    private lateinit var chbPlaying: CheckBox
-    private lateinit var chbTraveling: CheckBox
-    private lateinit var rbMale: RadioButton
-    private lateinit var rbFemale: RadioButton
-    private lateinit var skbPercentageBComBCA: SeekBar
-    private lateinit var btnSignup: Button
-    private lateinit var tvSelectedValues: TextView
-    private lateinit var tvPercentageSscHsc: TextView
-    private lateinit var tvPercentageBComBCA: TextView
     private var chbValue = ""
     private var rbGenderValue = ""
     private val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"
@@ -43,15 +25,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun init() {
-        spinnerCountry = findViewById(R.id.spinnerCountry)
-        spinnerSscHsc = findViewById(R.id.spinnerSscHsc)
-        spinnerBComBCA = findViewById(R.id.spinnerBComBCA)
-        tvSelectedValues = findViewById(R.id.tvSelectedValues)
-        rbMale = findViewById(R.id.rbMale)
-        rbFemale = findViewById(R.id.rbFemale)
-        chbReading = findViewById(R.id.chbReading)
-        chbPlaying = findViewById(R.id.chbPlaying)
-        chbTraveling = findViewById(R.id.chbTraveling)
         val dataSpinnerCountry = resources.getStringArray(R.array.Country)
         val adapterSpinnerCountry =
             ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, dataSpinnerCountry)
@@ -72,18 +45,6 @@ class MainActivity : AppCompatActivity() {
         )
         spinnerBComBCA.setPromptId(R.string.hint_Spinner_BCA_BCom)
         spinnerBComBCA.adapter = adapterSpinnerEducationTwo
-        tetFullName = findViewById(R.id.tetFullName)
-        tetEmailAddress = findViewById(R.id.tetEmailAddress)
-        tetBirthDate = findViewById(R.id.tetBirthDate)
-        tetAddress = findViewById(R.id.tetAddress)
-        tetPhoneNumber = findViewById(R.id.tetPhoneNumber)
-        tetBirthDate = findViewById(R.id.tetBirthDate)
-        skbPercentageSscHsc = findViewById(R.id.skbPercentageSscHsc)
-        tvPercentageBComBCA = findViewById(R.id.tvPercentageBComBca)
-        tvPercentageSscHsc = findViewById(R.id.tvPercentageSscHsc)
-        skbPercentageBComBCA = findViewById(R.id.skbPercentageBComBCA)
-        btnSignup = findViewById(R.id.btnSignup)
-
 
     }
 
@@ -109,59 +70,34 @@ class MainActivity : AppCompatActivity() {
         }
         btnSignup.setOnClickListener {
             if (tetFullName.text.toString().trim().isEmpty()) {
-                tetFullName.error = resources.getString(R.string.errorFullName)
+                tetFullName.error =getString(R.string.error_full_name)
                 tetFullName.requestFocus()
             } else if (tetPhoneNumber.text.toString().trim().isEmpty()) {
-                tetPhoneNumber.error = resources.getString(R.string.errorPhoneNumberIsEmpty)
+                tetPhoneNumber.error =getString(R.string.error_phone_number_is_empty)
                 tetPhoneNumber.requestFocus()
             } else if (tetPhoneNumber.text.toString().length != 10) {
-                tetPhoneNumber.error = resources.getString(R.string.errorPhoneNumberValid)
+                tetPhoneNumber.error =getString(R.string.error_phone_number_valid)
                 tetPhoneNumber.requestFocus()
 
             } else if (tetEmailAddress.text.toString().trim().isEmpty()) {
-                tetEmailAddress.error = resources.getString(R.string.errorEmailIsEmpty)
+                tetEmailAddress.error = getString(R.string.error_email_is_empty)
                 tetEmailAddress.requestFocus()
             } else if (!tetEmailAddress.text.toString().trim().matches(emailPattern.toRegex())) {
-                tetEmailAddress.error = resources.getString(R.string.errorEmailValid)
+                tetEmailAddress.error =getString(R.string.error_email_valid)
                 tetEmailAddress.requestFocus()
             } else if (tetAddress.text.toString().trim().isEmpty()) {
-                tetAddress.error = resources.getString(R.string.errorAddress)
+                tetAddress.error = getString(R.string.error_address)
                 tetAddress.requestFocus()
             } else if (tetBirthDate.text.toString().trim().isEmpty()) {
-                tetBirthDate.error = resources.getString(R.string.errorBirthIsEmpty)
+                tetBirthDate.error = getString(R.string.error_birth_is_empty)
                 tetBirthDate.requestFocus()
             } else {
-                if (chbReading.isChecked) {
-                    chbValue += "\n" + chbReading.text.toString()
-                }
-                if (chbPlaying.isChecked) {
-                    chbValue += "\n" + chbPlaying.text.toString()
-                }
-                if (chbTraveling.isChecked) {
-                    chbValue += "\n" + chbTraveling.text.toString()
-                }
-
-                rbGenderValue += if (rbMale.isChecked) {
-                    resources.getString(R.string.male)
-
-                } else {
-                    resources.getString(R.string.female)
-                }
-                var temp =
-                    resources.getString(R.string.selected_values) + "\n" + ":" + spinnerCountry.selectedItem.toString() + chbValue + "\n" + rbGenderValue + "\n" + spinnerSscHsc.selectedItem.toString()
-                temp += resources.getString(R.string.percentage) + ":" + skbPercentageSscHsc.progress.toString() + "%"
-                temp += "\n" + spinnerBComBCA.selectedItem.toString()
-                temp += resources.getString(R.string.percentage) + ":" + skbPercentageBComBCA.progress.toString() + "%"
-                tvSelectedValues.text = temp
-                temp = ""
-                rbGenderValue = ""
-                chbValue = ""
+                setSelectedValues()
             }
         }
         skbPercentageSscHsc.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                val seekbarData =
-                    resources.getString(R.string.percentage) + ":" + seekBar?.progress.toString() + "%"
+                val seekbarData =getString(R.string.percentage) + ":" + seekBar?.progress.toString() + "%"
                 tvPercentageSscHsc.text = seekbarData
 
             }
@@ -170,8 +106,7 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onStopTrackingTouch(seekBar: SeekBar?) {
-                val seekbarData =
-                    resources.getString(R.string.percentage) + ":" + seekBar?.progress.toString() + "%"
+                val seekbarData = getString(R.string.percentage) + ":" + seekBar?.progress.toString() + "%"
                 tvPercentageSscHsc.text = seekbarData
 
             }
@@ -179,9 +114,8 @@ class MainActivity : AppCompatActivity() {
         })
         skbPercentageBComBCA.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                val seekbarData =
-                    resources.getString(R.string.percentage) + ":" + seekBar?.progress.toString() + "%"
-                tvPercentageBComBCA.text = seekbarData
+                val seekbarData = getString(R.string.percentage) + ":" + seekBar?.progress.toString() + "%"
+                tvPercentageBComBca.text = seekbarData
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {
@@ -189,15 +123,39 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onStopTrackingTouch(seekBar: SeekBar?) {
-                val seekbarData =
-                    resources.getString(R.string.percentage) + ":" + seekBar?.progress.toString() + "%"
-                tvPercentageBComBCA.text = seekbarData
+                val seekbarData = getString(R.string.percentage) + ":" + seekBar?.progress.toString() + "%"
+                   tvPercentageBComBca.text = seekbarData
 
             }
 
         })
 
 
+    }
+    private fun setSelectedValues(){
+        if (chbReading.isChecked) {
+            chbValue += "\n" + chbReading.text.toString()
+        }
+        if (chbPlaying.isChecked) {
+            chbValue += "\n" + chbPlaying.text.toString()
+        }
+        if (chbTraveling.isChecked) {
+            chbValue += "\n" + chbTraveling.text.toString()
+        }
+
+        rbGenderValue = if (rbMale.isChecked) {
+            getString(R.string.male)
+
+        } else {
+            getString(R.string.female)
+        }
+        var tempSelectedValue = getString(R.string.selected_values) + "\n" + ":" + spinnerCountry.selectedItem.toString() + chbValue + "\n" + rbGenderValue + "\n" + spinnerSscHsc.selectedItem.toString()
+        tempSelectedValue += getString(R.string.percentage) + ":" + skbPercentageSscHsc.progress.toString() + "%"
+        tempSelectedValue += "\n" + spinnerBComBCA.selectedItem.toString()
+        tempSelectedValue += getString(R.string.percentage) + ":" + skbPercentageBComBCA.progress.toString() + "%"
+        tvSelectedValues.text = tempSelectedValue
+        rbGenderValue = ""
+        chbValue = ""
     }
 }
 
